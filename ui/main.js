9fb88d8a6a73e1d595a8ad1e642f05e54f62f22f
gokhu <30686054+gokhu@users.noreply.github.com>
2017-08-03 18:32:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,3 +10,49 @@ function moveright(){
 img.onclick = function() {
     var interval = setinterval(moveright,50);
 };
+//counter code
+var button = document.getElemenyById('counter')
+var counter = 0;
+button.onclick = function(){
+    //make a request to counter endpoint
+    var request = new XMLhttprequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readystate === XMLhttprequest.DONE) {
+            //take some action
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.tostring();
+            }
+        }
+    };
+//render the variable in the correct span
+//make the request
+request.open('GET', 'http://gokhu.imad.hasura-app.io/counter',true);
+request.send(null);
+};
+
+//submit name
+var nameinput = document.getElementById('name');
+var name = nameinput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    //make a request to a server and send the name
+    
+    
+    
+    
+    //capture the list of names and render it as a list
+    var names = ['name1','name2','name3','name4'];
+    var list ='';
+    for(var i=0;i<name.length;i++) {
+        list += '<li>' + name[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerhtml = list;
+};
+        
+    }
+}
