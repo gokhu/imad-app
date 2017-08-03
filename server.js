8eaa41a2cd03fb225b59da34af01b1739883bc4f
gokhu <30686054+gokhu@users.noreply.github.com>
2017-08-03 15:04:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,12 +4,55 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleone = {
+    title: 'articleone!gokhu',
+    heading: 'Articleone',
+    date: 'Sep 5 ,2016',
+    content: 
+    <p>
+    this is the content of first article.
+    </p>
+};
+function create Template (data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var html template =
+    <html>
+    <head>
+        <title>
+            $(title)
+        </title>
+        <link href="/ui/style.css" role="stylesheet"/>
+    </head>
+    <body>
+        <div>
+            <a href="/">home</a>
+        </div>
+        <hr/>
+        <h3>
+            $(heading)
+        </h3>
+        <div>
+            $(date)
+        </div>
+        <div>
+            <p>
+                $(content)
+            </p>
+        </div>
+    </body>
+</html>
+;
+return html template;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(create template(articleone));
 });
 
 app.get('/article-two', function (req, res) {
