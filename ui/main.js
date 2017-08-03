ef78f57c806fc4a6c64445e0fe32b9609d29f1e3
gokhu <30686054+gokhu@users.noreply.github.com>
2017-08-03 17:57:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+var element = document.getElementById('main-text');
+element.innerHTML = 'New Value';
+var img = document.getElementById('modi')
+var marginleft = 0;
+function moveright(){
+    marginleft = marginleft + 1;
+    img.style.marginleft = marginleft+'px';
+}
+img.onclick = function() {
+    var interval = setinterval(moveright,50);
+};
